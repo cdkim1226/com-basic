6817ca1a4b52b38ee74012b015a84ab7285d6ed7
cdkim1226 <cdkim1226@gmail.com>
2020-09-16 00:23:06 +0900

    댓글 조회 맵핑을 위한 Service

--- src/main/java/com/ncs/service/ReplyService.java
+++ src/main/java/com/ncs/service/ReplyService.java
@@ -1,5 +1,8 @@
 package com.ncs.service;
 
+import com.ncs.vo.ReplyVO;
+
 public interface ReplyService {
 
+	ReplyVO selectOne(ReplyVO replyVO);
 }
--- src/main/java/com/ncs/service/ReplyServiceImpl.java
+++ src/main/java/com/ncs/service/ReplyServiceImpl.java
@@ -1,8 +1,20 @@
 package com.ncs.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.ncs.mapper.ReplyMapper;
+import com.ncs.vo.ReplyVO;
 
 @Service
 public class ReplyServiceImpl implements ReplyService {
 
+	@Autowired
+	ReplyMapper replyMapper;
+
+	@Override
+	public ReplyVO selectOne(ReplyVO replyVO) {
+		return replyMapper.selectOne(replyVO);
+	}
+	
 }
