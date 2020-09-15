32b9fdafae1a81cfd403fa2e2ac7587c48cea4ee
cdkim1226 <cdkim1226@gmail.com>
2020-09-16 00:21:41 +0900

    댓글 crud를 위한 컨트롤러 셋팅

--- src/main/java/com/ncs/controller/ReplyController.java
+++ src/main/java/com/ncs/controller/ReplyController.java
@@ -1,10 +1,18 @@
 package com.ncs.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ncs.service.ReplyService;
 
 @RequestMapping(value = "/reply")
 @Controller
 public class ReplyController {
+	
+	@Autowired
+	ReplyService replyService;
+	
+	
 
 }
