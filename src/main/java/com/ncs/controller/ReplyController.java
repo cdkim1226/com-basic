6cddedbc45e4f27f265b8c7c8490ab1c9e35c82f
cdkim1226 <cdkim1226@gmail.com>
2020-09-18 16:07:37 +0900

    댓글 입력 컨트롤러

--- src/main/java/com/ncs/controller/ReplyController.java
+++ src/main/java/com/ncs/controller/ReplyController.java
@@ -3,8 +3,11 @@ package com.ncs.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.ncs.service.ReplyService;
+import com.ncs.vo.BoardVO;
+import com.ncs.vo.ReplyVO;
 
 @RequestMapping(value = "/reply")
 @Controller
@@ -14,5 +17,14 @@ public class ReplyController {
 	ReplyService replyService;
 	
 	
-
+	@RequestMapping(value = "/replyWrite", method = RequestMethod.POST)
+	public String replyWrite(ReplyVO rvo) {
+		replyService.insert(rvo);
+		return  ("redirect:/board/boardView?seq="+rvo.getSeq());
+	}
+	  
+	@RequestMapping(value = "/replyWrite", method = RequestMethod.GET)
+	public String getReplyWrite(BoardVO vo) {
+		return ("redirect:/board/boardView?seq="+vo.getSeq());
+	}
 }
