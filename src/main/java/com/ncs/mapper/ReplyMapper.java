51f8e9dc8ee4e5c35be3d6034913a0643c0c8e1f
cdkim1226 <cdkim1226@gmail.com>
2020-09-18 16:07:53 +0900

    댓글 조회 방식 mapper 변경

--- src/main/java/com/ncs/mapper/ReplyMapper.java
+++ src/main/java/com/ncs/mapper/ReplyMapper.java
@@ -1,10 +1,15 @@
 package com.ncs.mapper;
 
 
+
+import java.util.List;
+
 import com.ncs.vo.ReplyVO;
 
 public interface ReplyMapper {
 
-	ReplyVO selectOne(ReplyVO replyVO);
+	/* ReplyVO selectOne(ReplyVO replyVO); */
+	int insert(ReplyVO replyVO);
+	List<ReplyVO> selectList(int seq);
 
 }
