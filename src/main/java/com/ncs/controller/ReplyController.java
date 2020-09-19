022c72ac55f870b35f85f0c008cb98c9241a6a73
cdkim1226 <cdkim1226@gmail.com>
2020-09-19 23:31:20 +0900

    댓글 수정 컨트롤러

--- src/main/java/com/ncs/controller/ReplyController.java
+++ src/main/java/com/ncs/controller/ReplyController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.ncs.service.ReplyService;
 import com.ncs.vo.BoardVO;
@@ -25,5 +26,18 @@ public class ReplyController {
 	@RequestMapping(value = "/replyWrite", method = RequestMethod.GET)
 	public String getReplyWrite(BoardVO vo) {
 		return ("redirect:/board/boardView?seq="+vo.getSeq());
+	}
+	
+	@RequestMapping(value = "/modify", method = RequestMethod.POST)
+	public String postModify(ReplyVO rvo) {
+		replyService.modify(rvo);
+		return "redirect:/board/boardView?seq="+rvo.getSeq();
+	}
+	
+	@RequestMapping(value = "/modify", method = RequestMethod.GET)
+	public ModelAndView getModify(ModelAndView mv, ReplyVO rvo) {
+		mv.addObject("get",replyService.get(rvo));
+		mv.setViewName("jsonView");
+		return mv;
 	}
 }
