9945e4a1f75b989cd6d5896bc0a58d28e979576c
cdkim1226 <cdkim1226@gmail.com>
2020-09-23 02:25:56 +0900

    관리자 페이지 컨트롤러 생성

--- src/main/java/com/ncs/controller/ManagerController.java
+++ src/main/java/com/ncs/controller/ManagerController.java
@@ -5,8 +5,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+@RequestMapping(value = "/manager")
 @Controller
 public class ManagerController {
+	
+	@RequestMapping(value = "/dashBoard", method=RequestMethod.GET)
+	public ModelAndView dashBoard(ModelAndView mv) {
+		mv.setViewName("managerPage/dashBoard");
+		return mv;
+	}
 
 	@RequestMapping(value = "/user",method=RequestMethod.GET)
 	public ModelAndView user(ModelAndView mv) {
